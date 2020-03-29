f250e4bc33a7ac0178e5d214fe70159f4660d9f1
FareenaEhtesham <57524242+FareenaEhtesham@users.noreply.github.com>
2020-03-29 16:24:04 -0400

    Posts in detail

--- details.php
+++ details.php
@@ -62,7 +62,6 @@ while($row_posts=mysqli_fetch_array($run_posts))
     $post_author=$row_posts['post_author'];
     //$post_keyword=$row_posts['post_keyword'];
     $post_image=$row_posts['post_image'];
-    //$post_content=substr($row_posts['post_content'],0,100);
     $post_content=$row_posts['post_content'];
 
    echo"
